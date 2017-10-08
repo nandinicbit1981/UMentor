41362c717321a2d47791f4748fc7fdde3bdb2674
nandinicbit1981 <nandinicbit@gmail.com>
2017-10-08 16:45:07 -0700

    UI is now perfect!!!!!!

--- app/src/main/java/parimi/com/umentor/views/activity/MainActivity.java
+++ app/src/main/java/parimi/com/umentor/views/activity/MainActivity.java
@@ -71,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
         UMentorDaggerInjector.get().inject(this);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
         fcmToken = FirebaseInstanceId.getInstance().getToken();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mFirebaseAuth = FirebaseAuth.getInstance();
